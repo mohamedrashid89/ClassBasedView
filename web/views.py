3a6b3d33eba02dbd17ac37d0b37bd3f752490f7b
MOHAMED RASHID <mohamedrashidtp89@gmail.com>
2023-11-16 14:36:18 +0530

    first

---
+++ web/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView
+from .models import Product
+
+# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'web/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["produ"] = Product.objects.all()
+        return context
+
+class ProductDetails(DetailView):
+    template_name = 'web/details.html'
+    model = Product
